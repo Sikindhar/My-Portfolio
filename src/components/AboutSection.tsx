fd85532240fc1e956b33be2f5ef4a032592d2a04
Sikindhar <sikindharjaladi@gmail.com>
2025-07-14 21:52:02 +0530

    Update about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -19,12 +19,11 @@ const AboutSection = () => {
         
         <div>
           <h3 className="text-2xl font-bold mb-6 text-navy">
-            Final Year CS <span className="text-highlight">@SRM University , AP</span>
+            SDE Intern <span className="text-highlight">@Progress</span>
           </h3>
           
           <p className="text-gray-700 mb-6">
-            I am Computer Science Engineering student with a strong foundation in Problem-solving , Data structures , Algorithms and Software development. I enjoy researching new solutions to problems and like building iterative soltions to them.
-          </p>
+            I am Software development Engineer Intern at Progress on the ShareFile Product at Bengaluru. I am currently in the DocComponent team which is a major feature in the ShareFile application. </p>
           
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
             <Card className="card-hover-effect">
@@ -36,7 +35,7 @@ const AboutSection = () => {
             
             <Card className="card-hover-effect">
               <CardContent className="p-6">
-                <h4 className="font-bold text-navy mb-2">Amaravati</h4>
+                <h4 className="font-bold text-navy mb-2">SRM University, Amaravati</h4>
                 <p className="text-gray-600">Andhra Pradesh, India</p>
               </CardContent>
             </Card>
